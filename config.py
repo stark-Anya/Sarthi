9364a81fd0a3edc25c3389e95bd3da4bb8655f84
stark-Anya <misterxstark07@gmail.com>
2026-04-08 17:48:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+# Mister Starkieeee
+
 BOT_TOKEN  = ""
-ADMIN_ID   = 8275132868        # integer
-ADMIN_PASS = "anya"
-DB_PATH    = "jee_saarthi.db"
-TIMEZONE   = "Asia/Kolkata"
-DB_CLEAR_PASS = "stark-anya"
+ADMIN_ID   = 8275132868  # your owner id , 
+ADMIN_PASS = "Admin_pass" # set password for admin panell , any one can access admin panell to use this password !!...
+DB_PATH    = "jee_saarthi.db" # leave as same 
+TIMEZONE   = "Asia/Kolkata" # leave as same 
+DB_CLEAR_PASS = "db_clear_pass" # you can set same password as admin pass ! it will use for clear bot all data base !! 
