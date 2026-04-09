54ab3d8fd69286ca9b512a9aa949a5bdb301cbe2
stark-Anya <misterxstark07@gmail.com>
2026-04-09 16:38:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 BOT_TOKEN        = "YOUR_BOT_TOKEN"
-ADMIN_ID         = 123456789        # Your Telegram user ID (integer)
-ADMIN_PASS       = "yourpassword"   # Admin panel + Materials admin password
-DB_CLEAR_PASS    = "cleardb@123"    # Separate DB clear password (CHANGE THIS!)
+ADMIN_ID         = 8275132868        # Your Telegram user ID (integer)
+ADMIN_PASS       = "anya"   # Admin panel + Materials admin password
+DB_CLEAR_PASS    = "stark-anya"    # Separate DB clear password (CHANGE THIS!)
 DB_PATH          = "jee_saarthi.db"
 TIMEZONE         = "Asia/Kolkata"
-BACKUP_GROUP_ID  = -1001234567890   # Group or channel ID for daily DB backup
+BACKUP_GROUP_ID  = -1003758910410   # Group or channel ID for daily DB backup
                                     # Set your group ID here (negative number for groups)
