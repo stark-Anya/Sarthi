cb32e51008d0241f22d2ca2812102d7f03818404
stark-Anya <misterxstark07@gmail.com>
2026-04-09 16:23:40 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# Mister Starkieeee
-
-BOT_TOKEN      = "your_bot_token_here"       # From @BotFather
-ADMIN_ID       = 123456789                   # Your Telegram user ID
-ADMIN_PASS     = "your_admin_password"       # Admin panel password
-DB_CLEAR_PASS  = "your_db_clear_password"   # Separate clear DB password
-DB_PATH        = "jee_saarthi.db"
-TIMEZONE       = "Asia/Kolkata"
-BACKUP_GROUP_ID  = -1001234567890  
